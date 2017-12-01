c4f136fd6b20ccf326d71d59733f7d5b578dcb98
Marco <marco.moral89@gmail.com>
2017-12-01 00:14:31 -0800

    second commit server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express()
 const bodyParser = require('body-parser');
 const router = express.Router();
 
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -15,8 +17,14 @@ app.use('/todolist', router)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost/dadjokes/db')
+if(env === 'development') {
+        mongoose.connect('mongodb://localhost/dadjokes/db')
+} else {
+        mongoose.connect('mongodb://heroku_38lmg6p7:hrkg9r34mhstlls90jpm3gqdqg@ds125896.mlab.com:25896/heroku_38lmg6p7')
+}
+
 mongoose.Promise = global.Promise;
+
 
 
 const db = mongoose.connection;
@@ -32,7 +40,7 @@ let jokeListSchema = new Schema ({
 
 let jokeModel = mongoose.model('jokeModel', jokeListSchema);
 
-app.listen(8080)
+
 
 app.get('/jokelist', (req, res) => {
         jokeModel.find({})
@@ -53,3 +61,4 @@ app.post('/jokelist', (req,res) =>{
         
 })
 
+app.listen(8080)
